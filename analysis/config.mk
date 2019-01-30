e0f20738a247410774ceed10b8fae29f32fd5596
hachepunto <hachepunto@gmail.com>
2019-01-30 09:04:03 -0600

    I don't no remember what I do

--- analysis/config.mk
+++ analysis/config.mk
@@ -1,17 +1,22 @@
 #ddSeeker
-# ddSeeker number of cores
-NC=20
 # ddSeeker home
 DDS_HOME='/labs/genut/pkg/ddSeeker/'
+# ddSeeker Python environment
+DDS_ENV='/labs/usb/env/ddseeker/'
+# ddSeeker number of cores
+NC=20
 
 # Drop-seq tools home
 DST_HOME='/labs/genut/pkg/Drop-seq_tools-2.1.0/'
+# The annotations set to use to label the read.  This can be a GTF or a refFlat file.
+GTF='/remote/reference/labshare.cshl.edu/shares/gingeraslab/www-data/dobin/STAR/STARgenomes/ENSEMBL/homo_sapiens/ENSEMBL.homo_sapiens.release-75/Homo_sapiens.GRCh37.75.gtf'
 
 # Reference
-FASTA_REF=
+FASTA_REF='/remote/reference/labshare.cshl.edu/shares/gingeraslab/www-data/dobin/STAR/STARgenomes/ENSEMBL/homo_sapiens/ENSEMBL.homo_sapiens.release-75/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa'
 
 # STAR
 # Reference
-STAR_REFERENCEDIR=
+STAR_REFERENCEDIR='/remote/reference/labshare.cshl.edu/shares/gingeraslab/www-data/dobin/STAR/STARgenomes/ENSEMBL/homo_sapiens/ENSEMBL.homo_sapiens.release-75/'
 # STAR number fo proccesors
 NCSTAR=6
+
